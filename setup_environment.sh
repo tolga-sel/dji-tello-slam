ff142c12140e86b1177460b1c1c18d2244e48043
Tolga Sel <78278327+tolga-sel@users.noreply.github.com>
2021-08-29 17:29:09 +0000

    Update setup_environment.sh

--- setup_environment.sh
+++ setup_environment.sh
@@ -10,11 +10,11 @@ apt update -y
 apt upgrade -y
 
 # C++ Build tools
-apt install build-essential gdb
+apt install build-essential gdb -y
 
 # check for UTF-8
 locale
-sudo apt update && sudo apt install locales
+sudo apt update -y && sudo apt install locales -y
 sudo locale-gen en_US en_US.UTF-8
 sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
 export LANG=en_US.UTF-8
@@ -22,25 +22,25 @@ export LANG=en_US.UTF-8
 locale  # verify settings
 
 echo " - enable the Universe repository"
-sudo apt install software-properties-common
+sudo apt install software-properties-common -y
 sudo add-apt-repository universe
 apt-cache policy | grep universe
  500 http://us.archive.ubuntu.com/ubuntu focal/universe amd64 Packages
      release v=20.04,o=Ubuntu,a=focal,n=focal,l=Ubuntu,c=universe,b=amd64
 
 echo " - add the ROS 2 apt repository to your system"
-sudo apt update && sudo apt install curl gnupg lsb-release
+sudo apt update -y && sudo apt install curl gnupg lsb-release -y
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key  -o /usr/share/keyrings/ros-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 
 
 echo " - install ROS2 packages"
-sudo apt update
-sudo apt install ros-galactic-desktop
-sudo apt install ros-galactic-ros-base
+sudo apt update -y
+sudo apt install ros-galactic-desktop -y
+sudo apt install ros-galactic-ros-base -y
 
 source /opt/ros/galactic/setup.bash
-
+echo 'source /opt/ros/galactic/setup.bash' >> ~/.bashrc 
 
 #Install ROS 2
 echo " - Installing and initializing rosdep"
